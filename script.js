94eb1ab102d940a6e01c141405c4874addc8369d
Warren <the.waz88@gmail.com>
2024-06-04 10:01:39 +0200

    added choice results, 5 round game and console log results

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
+let humanScore;
+let computerScore;
 
-//Return a random number between 1 and 3.
-function getComputerChoice() { 
-    const num = Math.floor(Math.random() * 3 + 1) ;
-    return num === 1? "Rock": num === 2? "Paper": "Scissors"
+//Return a random number between 1 and 3, then assign ro Rock, Paper or Scissors.
+function getComputerChoice() {
+    const num = Math.floor(Math.random() * 3 + 1);
+    return num === 1 ? "Rock" : num === 2 ? "Paper" : "Scissors"
 }
 
 //Use prompt to ask the player to choose Rock, Paper or Scissors.
 function getHumanChoice() {
-    prompt("type Rock, Paper or Scissors to play")
+    return prompt("type Rock, Paper or Scissors to play", "Rock, Paper or Scissors?")
 }
 
-function playgame() {
-//Play 5 rounds and declare the winner.
-    let humanScore = 0;
-    let computerScore = 0;
+//Play a round to see who the winner is or if a tie
+function playRound() {
 
-    const playRound = function (humanChoice, computerChoice) {
-        //Return the winner of the current game.
-        getComputerChoice()
-        getHumanChoice()  
-    };
-    console.log(humanChoice + " " + computerChoice)
+    const humanChoice = getHumanChoice().toLowerCase();
+    const computerChoice = getComputerChoice();
+
+    //Check to see who wins and increment score, otherwise tie up
+    humanChoice == "rock" && computerChoice == "Paper" ||
+    humanChoice == "paper" && computerChoice == "Scissors" ||
+    humanChoice == "scissors" && computerChoice == "Rock" ? computerScore++ :
+    computerChoice == "Rock" && humanChoice == "paper" ||
+    computerChoice == "Paper" && humanChoice == "scissors" ||
+    computerChoice == "Scissors" && humanChoice == "rock" ? humanScore++ :
+    console.log("The round is a tie")
+
+    console.log("You picked " + humanChoice + " and the computer picked " + computerChoice + "!")
+};
+
+//Play 5 rounds and return winner/loser/tie
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+
+        playRound()
+        
+        console.log("You: " + humanScore + " - Computer: " + computerScore)
+    }
+    humanScore < computerScore ? console.log("You lost the game, " + humanScore + " to " + computerScore + ", press f5 to try again!") :
+    humanScore > computerScore ? console.log("You won the game!, " + humanScore + " to " + computerScore + ", press f5 to try again!") :
+    
+    console.log("No winner, press f5 to try again!")
+
 }
 
-playgame()
+//Makes it clearer to see when a new game starts
+console.log("=========================New Game===========================")
+
+//Starts the game
+playGame()
