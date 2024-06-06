2e8850e94b2e7ffff39ab5502df4a92c0d8917b3
Warren <the.waz88@gmail.com>
2024-06-06 15:15:15 +0200

    Adding html changes for button and body text additions

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+const resultText = document.getElementById("results");
+const buttons = document.getElementById("buttons")
+const scores = document.getElementById("scores")
+
+rock.addEventListener("click", () => playRound("Rock"));
+paper.addEventListener("click", () => playRound("Paper"));
+scissors.addEventListener("click", () => playRound("Scissors"));
+
 let humanScore = 0;
 let computerScore = 0;
+
+function checkEnd() {
+    humanScore == 5 ? buttons.innerText = "You won!" :
+    computerScore == 5 ? buttons.innerText = "You lost!" :
+    0;
+}
 
 //Return a random number between 1 and 3, then assign ro Rock, Paper or Scissors.
 function getComputerChoice() {
@@ -7,32 +25,19 @@ function getComputerChoice() {
     return num === 1 ? "Rock" : num === 2 ? "Paper" : "Scissors"
 }
 
-//Use prompt to ask the player to choose Rock, Paper or Scissors.
-function getHumanChoice() {
-    return prompt("type Rock, Paper or Scissors to play", "Rock, Paper or Scissors?")
-}
-
 //Play a round to see who the winner is or if a tie
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice) {
+    resultText.innerText = ""
+    const computerChoice = getComputerChoice()
 
-    const humanSelection = getHumanChoice().toLowerCase();
-    const computerSelection = getComputerChoice();
-    }
-    humanScore < computerScore ? console.log("You lost, " + humanScore + " to " + computerScore) :
-    humanScore > computerScore ? console.log("You won!, " + humanScore + " to " + computerScore) :  
-    console.log("No winner, press f5 to try again!")
-
-    //Check to see who wins and increment score, otherwise tie up
-    humanChoice == "rock" && computerChoice == "Paper" ||
-    humanChoice == "paper" && computerChoice == "Scissors" ||
-    humanChoice == "scissors" && computerChoice == "Rock" ? computerScore++ :
-    computerChoice == "Rock" && humanChoice == "paper" ||
-    computerChoice == "Paper" && humanChoice == "scissors" ||
-    computerChoice == "Scissors" && humanChoice == "rock" ? humanScore++ :
-    console.log("The round is a tie")
-
-    console.log("You picked " + humanChoice + " and the computer picked " + computerChoice + "!")
+    humanChoice == "Rock" && computerChoice == "Paper" ||
+    humanChoice == "Paper" && computerChoice == "Scissors" ||
+    humanChoice == "Scissors" && computerChoice == "Rock" ? computerScore++ :
+    computerChoice == "Rock" && humanChoice == "Paper" ||
+    computerChoice == "Paper" && humanChoice == "Scissors" ||
+    computerChoice == "Scissors" && humanChoice == "Rock" ? humanScore++ :
+    resultText.innerText = "The round is a tie, "
+    resultText.innerText += humanChoice + " versus " + computerChoice + "!"
+    scores.innerText = "You: " + humanScore + " - " + "Computer: " + computerScore
+    checkEnd()
 };
-
-//Starts the game
-playRound()
